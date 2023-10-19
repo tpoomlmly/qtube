2910f5beb8f14ffa3c9035cf4948db17b99e170c
Tomii Poll <tommy@poolle.co.uk>
2023-10-19 21:41:20 +0100

    Try extracting info directly

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import threading
 import yt_dlp
 from flask import Flask, render_template, request, abort
 from werkzeug.exceptions import HTTPException
+from yt_dlp import YoutubeDL
 from yt_dlp.extractor.common import InfoExtractor
 from yt_dlp.extractor.youtube import YoutubeIE, YoutubeYtBeIE
+from yt_dlp.utils import YoutubeDLError
 
 app = Flask(__name__)
 
@@ -19,10 +21,27 @@ def is_youtube_url(url: str) -> bool:
 
 @app.route('/', methods=['GET'])
 def index() -> str:
-    # url = request.args.get("url")
-    # print(url)
-    # if not is_youtube_url(url):
-    #     abort(400, "youtube links only pls")
+    url = request.args.get("v")
+    print(url)
+
+    if url is None:
+        return render_template("index.html")
+
+    if not is_youtube_url(url):
+        abort(400, "youtube links only pls")
+
+    print(YoutubeIE.extract_id(url))
+
+    parser, opts, all_urls, ydl_opts = yt_dlp.parse_options(argv=None)
+    with YoutubeDL(ydl_opts) as youtube_dl:
+        parser.destroy()
+        try:
+            # status_code: int = youtube_dl.download(url)
+            video_info = youtube_dl.extract_info(url, download=False)
+            print(f"id: {fmt['format_id']}, video: {fmt['vcodec']}, audio: {fmt['acodec']}" for fmt in video_info.get("formats"))
+            # print(youtube_dl.__download_wrapper(youtube_dl.extract_info)(url, download=False))
+        except YoutubeDLError:
+            abort(500, "download cancelled :(")
 
     return render_template("index.html")
 
